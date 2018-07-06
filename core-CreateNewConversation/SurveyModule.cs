2e967f89b0afde55df3b3b70814a4f0848a4523a
Jason Sowers <v-jassow@microsoft.com>
2018-07-06 14:02:16 -0700

    initial commit

---
+++ core-CreateNewConversation/SurveyModule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Bot.Builder.Azure;
+
+namespace CreateNewConversationBot
+{
+    using Autofac;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.Dialogs.Internals;
+    using Microsoft.Bot.Builder.Internals.Fibers;
+    using Microsoft.Bot.Connector;
+
+    public class SurveyModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            try
+            {
+                base.Load(builder);
+                var store =
+                    new TableBotDataStore(ConfigurationManager.ConnectionStrings["AzureStorage"].ConnectionString);
+
+                builder.Register(c => store)
+                    .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                    .AsSelf()
+                    .SingleInstance();
+
+                builder.Register(c => new CachingBotDataStore(store,
+                        CachingBotDataStoreConsistencyPolicy
+                            .ETagBasedConsistency))
+                    .As<IBotDataStore<BotData>>()
+                    .AsSelf()
+                    .InstancePerLifetimeScope();
+
+
+                builder.RegisterType<CreateNewConversationDialog>().As<IDialog<object>>().InstancePerDependency();
+
+                builder.Register((c, p) => new SurveyDialog()).AsSelf().InstancePerDependency();
+
+                builder.RegisterType<SurveyScheduler>().Keyed<ISurveyScheduler>(FiberModule.Key_DoNotSerialize)
+                    .AsImplementedInterfaces().SingleInstance();
+
+                builder.Register(c => new SurveyService(c.Resolve<ISurveyScheduler>(), c.Resolve<ConversationReference>()))
+                    .Keyed<ISurveyService>(FiberModule.Key_DoNotSerialize).AsImplementedInterfaces()
+                    .InstancePerMatchingLifetimeScope(DialogModule.LifetimeScopeTag);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
